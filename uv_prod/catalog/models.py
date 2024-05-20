853623a2760df61ff19f83cb3c52e3df339ca648
Maria Sharkova <moriko.tys@gmail.com>
2024-05-20 15:20:44 +0300

    migr

--- uv_prod/catalog/models.py
+++ uv_prod/catalog/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 
 class Component(models.Model):
-    name = models.CharField(max_length=50, verbose_name='')
-    description = models.TextField(blank=True, null=True, verbose_name='')
-    quantity = models.PositiveIntegerField(verbose_name='')
-    critical_quantity = models.PositiveIntegerField(blank=True, null=True, verbose_name='')
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(
+        blank=True, null=True, verbose_name='Описание'
+    )
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество на складе'
+    )
+    critical_quantity = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        verbose_name='Критическое количество, когда пора заказывать'
+    )
 
     def __str__(self):
         return f'{self.name}, {self.quantity} шт. на складе'
