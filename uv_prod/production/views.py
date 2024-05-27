bc25f335c28a197c243c4161718d654a48caa046
Maria Sharkova <moriko.tys@gmail.com>
2024-05-27 18:05:15 +0300

    optimise view

--- uv_prod/production/views.py
+++ uv_prod/production/views.py
@@ -5,15 +5,17 @@ from production.models import Board, Production, StageComponentBoardQuantity
 
 def production(request):
     template_name = 'production/production.html'
-    board_list = Board.objects.all()
+    board_list = Board.objects.values('slug', 'name')
+    production_data = Production.objects.select_related(
+        'board'
+    ).values('board__slug', 'quantity')
+
     for board in board_list:
-        production_quantity = 0
-        production_data = Production.objects.filter(board=board)
-
-        for production_instance in production_data:
-            production_quantity += production_instance.quantity
-
-        board.total_production_quantity = production_quantity
+        production_quantity = sum(
+            data['quantity'] for data in production_data if (
+                data['board__slug'] == board['slug'])
+        )
+        board['total_production_quantity'] = production_quantity
     context = {
         'board_list': board_list,
     }
@@ -23,25 +25,17 @@ def production(request):
 def board_production(request, slug):
     template_name = 'production/board-base.html'
     board = get_object_or_404(Board, slug=slug)
-    board_list = Board.objects.all()
-    production_quantity = 0
+    board_list = Board.objects.values('slug', 'name')
     production_data = Production.objects.filter(board=board)
 
-    for production_instance in production_data:
-        production_quantity += production_instance.quantity
-
-    board.total_production_quantity = production_quantity
-
     combined_data = []
+    stage_components = StageComponentBoardQuantity.objects.filter(
+        board=board
+    ).select_related('stage')
 
-    for stage in StageComponentBoardQuantity.objects.filter(board=board):
-        production = Production.objects.filter(
-            board=board, stage=stage.stage
-        ).first()
-        if production:
-            quantity = production.quantity
-        else:
-            quantity = 0
+    for stage in stage_components:
+        production = production_data.filter(stage=stage.stage).first()
+        quantity = production.quantity if production else 0
         combined_data.append(
             {
                 'stage': stage.stage,
@@ -49,6 +43,7 @@ def board_production(request, slug):
                 'cable': stage.stage.cable_stage,
             }
         )
+
     total_quantity = sum(data['quantity'] for data in combined_data)
     context = {
         'board_list': board_list,
