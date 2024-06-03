1cdd76997c1c13311f4e59f5c4dbf2420fd57fb0
Maria Sharkova <moriko.tys@gmail.com>
2024-06-03 18:08:06 +0300

    form check

--- uv_prod/production/forms.py
+++ uv_prod/production/forms.py
@@ -1,6 +1,7 @@
+from typing import Any
 from django import forms
 
-from production.models import Production
+from production.models import Production, Stage
 
 
 class ProductionFormtest(forms.ModelForm):
@@ -32,3 +33,24 @@ class ProductionForm(forms.Form):
     )
     hidden_board = forms.CharField(widget=forms.HiddenInput)
     hidden_stage = forms.CharField(widget=forms.HiddenInput)
+
+    def clean(self):
+        '''
+        Проверка максимального количества.
+        Работает только если в бд заведены записи производства.
+        '''
+        cleaned_data = super().clean()
+        hidden_stage = cleaned_data.get('hidden_stage')
+        current_stage = Stage.objects.get(name=hidden_stage).order
+        print(current_stage)
+
+        if hidden_stage:
+            previous_stage = Stage.objects.filter(
+                order__lt=current_stage
+            ).first()
+
+            if previous_stage:
+                max_quantity = previous_stage.production_set.first().quantity
+                self.fields['quantity'].widget.attrs['max'] = max_quantity
+
+        return cleaned_data
