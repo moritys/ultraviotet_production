8d8b11e37b5e1ce03d5f36e2cd3b5123dc4d498e
Maria Sharkova <moriko.tys@gmail.com>
2024-08-02 17:39:17 +0300

    dock

--- uv_prod/uv_prod/settings.py
+++ uv_prod/uv_prod/settings.py
@@ -13,6 +13,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.99', 'localhost', '127.0.0.1', 'web']
 
+CSRF_TRUSTED_ORIGINS = [
+    'ALLOWED_HOSTS'
+]
+
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
