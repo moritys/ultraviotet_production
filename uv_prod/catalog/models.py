c6d0485b7fa366257763aa29f0b3817e78ea07d7
Maria Sharkova <moriko.tys@gmail.com>
2024-08-06 15:57:52 +0300

    uniq_comp

--- uv_prod/catalog/models.py
+++ uv_prod/catalog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Component(models.Model):
     name = models.CharField(
+        unique=True,
         max_length=50,
         verbose_name='Название',
         help_text='Схема названия: "Part | Footprint"'
